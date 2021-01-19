3ec54f9f105e85eebf7b7982e361ee473829e61e
Jae <44139594+sat8n@users.noreply.github.com>
2021-01-19 21:53:23 +0800

    Discord bot file

---
+++ bot.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+
+import discord
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+@bot.command(name='test_bot')
+async def test(ctx):
+    await ctx.send("Test successful.")
+
+bot.run(TOKEN)

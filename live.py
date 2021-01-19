576944e035b7d7f2f066d421ba185e84702c6b86
Jae <44139594+sat8n@users.noreply.github.com>
2021-01-19 19:49:39 +0800

    Methods that communicate with Twitch API

---
+++ live.py
@@ -0,0 +1,73 @@
+import urllib.request
+import os
+from dotenv import load_dotenv
+
+from twitchAPI.twitch import Twitch
+
+load_dotenv()
+CLIENT_ID = os.getenv('TWITCH_CLIENT_ID')
+SECRET = os.getenv('TWITCH_SECRET')
+
+twitch = Twitch(CLIENT_ID, SECRET)
+twitch.authenticate_app([])
+
+# getUserID(STREAMERS[0])['id'] to get id
+def getUserID(streamer):
+    user_info = twitch.get_users(logins=streamer)
+    user_data = user_info['data'][0]
+    return user_data
+
+def getChannel(streamer_id):
+    channel_info = twitch.get_channel_information(streamer_id)
+    return channel_info
+
+# this will return a filled data array if a steamer is live
+def getLive(streamer_id):
+    stream_info = twitch.get_streams(user_id=streamer_id)
+    return stream_info
+
+### TO DO: move this to a separate text file for the program to read from
+STREAMER_DICTIONARY = { # streamer: [isLive, notificationSent]
+    "sodapoppin": [False, False],
+    "anton": [False, False],
+    "MOONMOON": [False, False],
+    "summit1g": [False, False],
+    "mitchjones": [False, False],
+    "watchmeblink": [False, False],
+    "spoonkid": [False, False],
+    "dafran": [False, False]
+}
+
+# checkStreamers(list(STREAMER_DICTIONARY.keys()))
+def checkStreamers(streamers):
+    for streamer in streamers:
+        streamer_id = getUserID(streamer)['id']
+        streamer_info = getLive(streamer_id)
+        if streamer_info['data']: # if a streamer is live
+            STREAMER_DICTIONARY[streamer][0] = True
+            if STREAMER_DICTIONARY[streamer][1] == False: # if notified is false
+                print(streamer + " is live") ### change this part to discord bot
+                STREAMER_DICTIONARY[streamer][1] = True
+        elif not streamer_info['data']: # if a streamer is not live
+            if STREAMER_DICTIONARY[streamer][0] == True: # if they were live before
+                print(streamer + " has gone offline")
+                STREAMER_DICTIONARY[streamer][0] = False # change to false
+                STREAMER_DICTIONARY[streamer][1] = False
+            else:
+                STREAMER_DICTIONARY[streamer][0] = False # keep it at false
+                STREAMER_DICTIONARY[streamer][1] = False
+
+### this is where we loop forever
+# import schedule
+# import time
+
+# def job():
+#     print("Checking for live streamers...")
+#     STREAMERS = list(STREAMER_DICTIONARY.keys())
+#     checkStreamers(STREAMERS)
+
+# schedule.every(1).minutes.do(job)
+
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)

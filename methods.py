48a2bed1692e390605a005f5902fff7c084f6810
Jae <44139594+sat8n@users.noreply.github.com>
2021-01-21 23:20:49 +0800

    Method to write to the JSON file

--- methods.py
+++ methods.py
@@ -5,3 +5,8 @@ def openJSON():
     file = open('streamers.json')
     STREAMER_DICTIONARY = json.load(file)
     return STREAMER_DICTIONARY
+
+def writeJSON(dictionary):
+    new_json = json.dumps(dictionary, indent=4)
+    with open('streamers.json', 'w') as outfile:
+        outfile.write(new_json)
